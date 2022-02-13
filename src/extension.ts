6cdc77cf24df1ceb63f4d6f073c341fe975c3a15
HomyeeKing <HomyeeKing@gmail.com>
2022-02-13 10:57:40 +0800

    chore: refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { checkUrl } from "./utils";
+import mediaDisposal from './media-preview';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,64 +9,9 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "preview-link" is now active!');
-  const disposal = vscode.languages.registerHoverProvider(
-    [
-      "javascript",
-      "typescript",
-      "javascriptreact",
-      "typescriptreact",
-      "json",
-      "jsonc",
-    ],
-    {
-      async provideHover(document, position) {
 
-        console.log('preview link in');
-        
-        const urlRe =
-          /(https?:)?\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/=,\*!]*)/gi;
 
-        const range = document.getWordRangeAtPosition(position, urlRe);
-        if (range) {
-          let url = document.getText(range);
-          if (url.startsWith("//")) {
-            url = `https:${url}`;
-          }
-          url = encodeURI(url);
-          console.log('url', url);
-          const mdStr = new vscode.MarkdownString();
-          mdStr.supportHtml = true;
-          mdStr.value = "";
-          const urlType = await checkUrl(url);
-          console.log('urlType', urlType);
-          switch (urlType.type) {
-            case "img":
-              mdStr.value = `![hover-link-image](${url})`;
-              break;
-            // case "audio":
-            //   mdStr.value = `<audio controls src='${url}' >
-            //     vscode doesn't support audio, you can click Follow link for more details
-            //   </audio>`;
-            //   break;
-            // case "video":
-            //   mdStr.value = `<video controls src='${url}' width="250">
-            //    vscode doesn't support video, you can click Follow link for more details
-            //   </video>`;
-            //   break;
-            case 'invalid':
-              mdStr.value = `the matched url is ${url}, please file an issue [here](${require('../package.json').bugs.url})`;
-              break;
-            default:
-              break;
-          }
-
-          return new vscode.Hover(mdStr);
-        }
-      },
-    }
-  );
-
-  context.subscriptions.push(disposal);
+  context.subscriptions.push(mediaDisposal);
 }
 
 // this method is called when your extension is deactivated
